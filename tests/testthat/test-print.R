9006479e91e95fa3b9e4684b710070b40982b81a
Lukasz A. Bartnik <l.bartnik@gmail.com>
2018-09-07 16:50:18 -0700

    rename output file

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -3,7 +3,7 @@ context("print")
 a <- read_artifacts(as_artifacts(london_meters()))
 
 test_that("print tree", {
-  expect_output_file(print(new_tree(a)), "text-output/print-origin-tree.txt")
+  expect_output_file(print(new_tree(a)), "text-output/print-tree.txt")
 })
 
 test_that("print history", {
