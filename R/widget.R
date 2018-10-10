b569431919485672d0058bd1a23b931fbc44c4fd
Lukasz A. Bartnik <l.bartnik@gmail.com>
2018-10-10 01:43:05 -0700

    add .inBrowser option to browser addin binding

--- R/widget.R
+++ R/widget.R
@@ -2,7 +2,7 @@
 #'
 #' @export
 #' @importFrom browser browser_addin
-browser_addin_binding <- function () {
+browser_addin_binding <- function (.inBrowser = FALSE) {
   data <- read_artifacts(as_artifacts(state$repo))
-  browser_addin(data)
+  browser_addin(data, .inBrowser)
 }
